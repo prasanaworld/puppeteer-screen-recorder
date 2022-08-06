863f5fcb22d85b915e6fb2f479de329e142b760a
prasanaworld <prasanaworld@gmail.com>
2022-08-06 17:47:41 +0100

    updated example

--- src/example/index.ts
+++ src/example/index.ts
@@ -2,17 +2,26 @@ import puppeteer from 'puppeteer';
 
 import { PuppeteerScreenRecorder } from '../lib/PuppeteerScreenRecorder';
 
+function sleep(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 /**
  * @ignore
  */
 async function executeSample(format) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: process.env['PUPPETEER_EXECUTABLE_PATH'],
+  });
   const page = await browser.newPage();
   const recorder = new PuppeteerScreenRecorder(page);
   await recorder.start(format);
   await page.goto('https://yahoo.com');
-
+  await sleep(3000);
   await page.goto('https://google.com');
+  await sleep(3000);
   await recorder.stop();
   await browser.close();
 }
