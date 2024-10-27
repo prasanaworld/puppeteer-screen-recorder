9962380382484a7886bd14cab61643553d334419
Prasana world <prasanaworld@gmail.com>
2024-10-27 18:35:34 +0000

    fixing prettier issue and adding new publish workflow

--- src/spec/PuppeteerScreenRecorder.spec.ts
+++ src/spec/PuppeteerScreenRecorder.spec.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { dirname } from 'path';
 
-import {describe, expect, it} from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import puppeteer from 'puppeteer';
 
 import { PuppeteerScreenRecorder, PuppeteerScreenRecorderOptions } from '../';
@@ -21,126 +21,126 @@ describe('Happy path test case', () => {
   it('Should be able to create a new screen-recording session', async () => {
     /** setup */
     const browser = await launchBrowser();
-  
+
     const page = await browser.newPage();
-  
+
     const outputVideoPath = './test-output/test/video-recorder/testCase1.mp4';
     const recorder = new PuppeteerScreenRecorder(page);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     await page.goto('https://google.com', { waitUntil: 'load' });
-  
+
     /** clear */
     const status = await recorder.stop();
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('Should be able to create a new screen-recording session using mp4 format', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const outputVideoPath = './test-output/test/video-recorder/testCase1.mp4';
     const recorder = new PuppeteerScreenRecorder(page);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     await page.goto('https://google.com', { waitUntil: 'load' });
-  
+
     /** clear */
     const status = await recorder.stop();
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('Should be able to create a new screen-recording session using mov', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const outputVideoPath = './test-output/test/video-recorder/testCase1.mov';
     const recorder = new PuppeteerScreenRecorder(page);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     await page.goto('https://google.com', { waitUntil: 'load' });
-  
+
     /** clear */
     const status = await recorder.stop();
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('Should be able to create a new screen-recording session using webm', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const outputVideoPath = './test-output/test/video-recorder/testCase1.webm';
     const recorder = new PuppeteerScreenRecorder(page);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     await page.goto('https://google.com', { waitUntil: 'load' });
-  
+
     /** clear */
     const status = await recorder.stop();
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('should be to get the total duration of recording using avi', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const outputVideoPath = './test-output/test/video-recorder/testCase2.avi';
     const recorder = new PuppeteerScreenRecorder(page);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
-  
+
     await page.goto('https://google.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
+
     recorder.getRecordDuration();
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('should be able to record a video with video frame width, height and aspect ratio', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const options: PuppeteerScreenRecorderOptions = {
       followNewTab: false,
       videoFrame: {
@@ -152,20 +152,19 @@ describe('Happy path test case', () => {
     const outputVideoPath = './test-output/test/video-recorder/testCase4.mp4';
     const recorder = new PuppeteerScreenRecorder(page, options);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
-    await browser.close();
-  
-    /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
-  });
 
+    await browser.close();
+
+    /** assert */
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
+  });
 
   it('Should be able to create a new screen-recording session using streams', async () => {
     /** setup */
@@ -193,23 +192,20 @@ describe('Happy path test case', () => {
     await browser.close();
 
     /** assert */
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
     fileWriteStream.on('end', () => {
-      expect(fileWriteStream.writableFinished).toBeTruthy()
+      expect(fileWriteStream.writableFinished).toBeTruthy();
     });
   });
 });
 
-
-
-
-describe("video frame testing", () => {
+describe('video frame testing', () => {
   it('testing video recording with video frame width, height and autopad color', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const options: PuppeteerScreenRecorderOptions = {
       followNewTab: false,
       videoFrame: {
@@ -223,25 +219,25 @@ describe("video frame testing", () => {
     const outputVideoPath = './test-output/test/video-recorder/testCase5a.mp4';
     const recorder = new PuppeteerScreenRecorder(page, options);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
+
     await browser.close();
-  
+
     /** assert */
     expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
     expect(status).toBeTruthy();
     expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('testing video recording with video frame width, height and autopad color as hex code', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const options: PuppeteerScreenRecorderOptions = {
       followNewTab: false,
       videoFrame: {
@@ -255,25 +251,25 @@ describe("video frame testing", () => {
     const outputVideoPath = './test-output/test/video-recorder/testCase5b.mp4';
     const recorder = new PuppeteerScreenRecorder(page, options);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
+
     await browser.close();
-  
+
     /** assert */
     expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
     expect(status).toBeTruthy();
     expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-  
+
   it('testing video recording with video frame width, height and default autopad color', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const options: PuppeteerScreenRecorderOptions = {
       followNewTab: false,
       videoFrame: {
@@ -285,14 +281,14 @@ describe("video frame testing", () => {
     const outputVideoPath = './test-output/test/video-recorder/testCase5c.mp4';
     const recorder = new PuppeteerScreenRecorder(page, options);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
+
     await browser.close();
-  
+
     /** assert */
     expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
     expect(status).toBeTruthy();
@@ -300,19 +296,19 @@ describe("video frame testing", () => {
   });
 });
 
-describe("Defensive test", () => {
+describe('Defensive test', () => {
   it('should throw error if an invalid savePath argument is passed for start method', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     try {
       const outputVideoPath = './test-output/test/video-recorder/';
       const recorder = new PuppeteerScreenRecorder(page);
       await recorder.start(outputVideoPath);
       /** execute */
       await page.goto('https://github.com', { waitUntil: 'load' });
-  
+
       await page.goto('https://google.com', { waitUntil: 'load' });
       /** clear */
       await recorder.stop();
@@ -320,12 +316,12 @@ describe("Defensive test", () => {
       expect(error.message === 'File format is not supported').toBeTruthy();
     }
   });
-  
+
   it('should create a video with a custom crf', async () => {
     /** setup */
     const browser = await launchBrowser();
     const page = await browser.newPage();
-  
+
     const options: PuppeteerScreenRecorderOptions = {
       followNewTab: false,
       videoFrame: {
@@ -337,19 +333,17 @@ describe("Defensive test", () => {
     const outputVideoPath = './test-output/test/video-recorder/testCase6.mp4';
     const recorder = new PuppeteerScreenRecorder(page, options);
     const recorderValue = await recorder.start(outputVideoPath);
-  
+
     /** execute */
     await page.goto('https://github.com', { waitUntil: 'load' });
     /** clear */
     const status = await recorder.stop();
-  
+
     await browser.close();
-  
+
     /** assert */
-    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy()
-    expect(status).toBeTruthy()
-    expect(fs.existsSync(outputVideoPath)).toBeTruthy()
+    expect(recorderValue instanceof PuppeteerScreenRecorder).toBeTruthy();
+    expect(status).toBeTruthy();
+    expect(fs.existsSync(outputVideoPath)).toBeTruthy();
   });
-
 });
-
